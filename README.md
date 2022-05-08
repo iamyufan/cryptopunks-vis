ed0832e1b70daa57a36cb705f9f7d309d8d91bb9
Yufan Zhang <brucezzzzzzz@outlook.com>
2022-05-09 00:24:41 +0800

    Update

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
 # STATS401-Project2
- Repo for DKU STATS401 Project 2 2022 Spring
+
+This project is trying to explore the attributes and prices of CryptoPunk, one of the most popular 
+NFT project around the world, via data visualization.
+
+## Repo structure
+
+```
+project
+│   README.md
+│   requirements.txt
+│   app.py
+│   utils.py
+│
+└───assets
+│   │   style.css
+│   │   logo.csv
+│
+└───data
+│   │   vis1_data.json
+│   │   vis2_data.csv
+│   │   vis3_data.csv
+│   │   vis4_data.csv
+│   │   vis5_data.csv
+│   
+└───data_main
+│   │   queryDune.py
+│   │   scrapeTweets.ipynb
+│   │   database.ipynb
+│   │
+│   └───vis_data_clean
+│   │   │   vis1.ipynb
+│   │   │   vis2.ipynb
+│   │   │   vis3.ipynb
+│   │   │   vis4.ipynb
+│   │   │   vis5.ipynb
+│   │
+│   └───_data
+│   │   │   punkBought_{date}.json
+│   │   │   punkTransfer_{date}.json
+│   │
+│   └───database
+│       │   punk_db.csv
+│       │   trader_db.csv
+│       │   tweets_db.csv
+│       │   tx_db.csv
+```
---
+++ data_main/queryDune.py
@@ -0,0 +1,63 @@
+from duneanalytics import DuneAnalytics
+import time
+import json
+
+
+def query_data_from_dune(data_mode):
+    """
+    Query data from Dune Analytics
+    """
+    # initialize client
+    dune = DuneAnalytics('brucezzzzzzzyf@gmail.com', 'ZHANG0509yf')
+
+    # try to login
+    dune.login()
+
+    # fetch token
+    dune.fetch_auth_token()
+
+    # set query ids
+    if data_mode == 'punkBought':
+        query_dict = {'2017': 673785,
+                      '2018': 673784,
+                      '2019': 673783,
+                      '2020': 673781,
+                      '2021': 673779,
+                      '2022': 673771}
+    elif data_mode == 'punkTransfer':
+        query_dict = {'2017-2019': 674456,
+                      '2020': 674472,
+                      '2021': 674496,
+                      '2022': 674500}
+
+    # query data
+    dune_data = {}
+    for year in query_dict:
+        query_id = query_dict[year]
+        result_id = dune.query_result_id(query_id=query_id)
+        dune_data[year] = dune.query_result(result_id)
+        time.sleep(5)
+
+    return dune_data
+
+
+DUNE_DATA_PATH = './dune_data'
+
+
+def save_data(dune_dict, file_predix, DUNE_DATA_PATH=DUNE_DATA_PATH):
+    """
+    Write data to json file
+    """
+    from datetime import date
+    today = str(date.today())
+    out_file = '{}/{}_{}.json'.format(DUNE_DATA_PATH, file_predix, today)
+    with open(out_file, 'w') as f:
+        json.dump(dune_dict, f)
+    print('===== Data saved to {} ====='.format(out_file))
+
+
+dune_bt_dict = query_data_from_dune('punkBought')
+save_data(dune_bt_dict, 'punkBought')
+
+dune_tf_dict = query_data_from_dune('punkTransfer')
+save_data(dune_tf_dict, 'punkTransfer')
